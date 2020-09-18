017dc297dc9ba748415f0bbf2e55eecede75c528
Diogo Martins-de-Sa <sadiogo@mol.bio.br>
2020-09-18 13:07:09 -0300

    finished yelp-mvp exercise

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,8 +5,13 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    Review.create(review_params)
-    redirect_to restaurant_path(params[:restaurant_id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to restaurant_path(params[:restaurant_id])
+    else
+      render :new
+    end
   end
 
   private
